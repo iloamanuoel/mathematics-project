ed0f9dcf38443e5fbebccfb2867f5ff8223e2d8c
iloamanuoel <iloamanuoel@gmail.com>
2025-03-11 18:08:58 +0000

    Add 7.cpp

---
+++ src/7.cpp
@@ -0,0 +1,8 @@
+int main() {
+    int n = 5;
+    double x = 2.0;
+    double y = 3.0;
+    double z = (x + y) / n;
+    cout << "The average of " << x << " and " << y << " is " << z << endl;
+    return 0;
+}
