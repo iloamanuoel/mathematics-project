be9cf4d6aa73e92f263f6102b92bfa300d017e6f
iloamanuoel <iloamanuoel@gmail.com>
2025-03-15 14:22:34 +0000

    Add 10.cpp

---
+++ src/10.cpp
@@ -0,0 +1,7 @@
+int main(){
+    cout<<"Enter two numbers to add: ";
+    int num1, num2;
+    cin>>num1>>num2;
+    cout<<"The sum of "<<num1<<" and "<<num2<<" is "<<(num1+num2)<<endl;
+    return 0;
+}
