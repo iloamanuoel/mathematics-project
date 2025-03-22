23dff79b3898396defcea8fe3e06facf89fef5c3
iloamanuoel <iloamanuoel@gmail.com>
2025-03-22 14:50:52 +0000

    Add 15.cpp

---
+++ src/15.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    if (x > 5) {
+        cout << "The value of x is greater than 5." << endl;
+    } else {
+        cout << "The value of x is not greater than 5." << endl;
+    }
+    return 0;
+}
