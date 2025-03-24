8d3d951bdc2e773fb2ea53fd6263ad003650c6b9
iloamanuoel <iloamanuoel@gmail.com>
2025-03-24 02:38:04 +0000

    Add 17.cpp

---
+++ src/17.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    
+    if (num1 > num2) {
+        cout << "Number 1 is greater than Number 2." << endl;
+    } else {
+        cout << "Number 1 is not greater than Number 2." << endl;
+    }
+    
+    return 0;
+}
