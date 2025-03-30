31862717cec050de3965d9627d948b42c9ee542e
iloamanuoel <iloamanuoel@gmail.com>
2025-03-30 22:28:52 +0000

    Add 19.cpp

---
+++ src/19.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    if (x > y) {
+        cout << "The value of y is larger." << endl;
+    } else {
+        cout << "The value of x is larger." << endl;
+    }
+    return 0;
+}
