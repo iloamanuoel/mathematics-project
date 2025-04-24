eb74e07d19cd595b730d34b173210aaa2812966a
iloamanuoel <iloamanuoel@gmail.com>
2025-04-24 04:51:33 +0000

    Add 30.cpp

---
+++ src/30.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int c = 20;
+    
+    if (a > b) {
+        cout << "a is greater than b" << endl;
+    } else {
+        cout << "b is greater than a" << endl;
+    }
+    
+    return 0;
+}
